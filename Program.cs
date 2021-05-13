a4d78069330ff40eddc4084cd42ea54406941a3b
Daniel Castel De Oro <daniel.castel@live.com>
2021-05-13 04:42:10 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SayHi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Say hi! Don't be shy.");
+        }
+    }
+}
